1e65ad00fcd4188dc14163e74b23057f9568f599
TicSide <omarriveraticllacuri@gmail.com>
2025-04-01 11:28:45 -0500

    Adding Camera Shake

---
+++ LagsGameJam/Assets/Scripts/Camera/ExplosionTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionTrigger : MonoBehaviour
+{
+    public ParticleSystem explosio;
+    public CameraShake cameraShake;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            explosio.Play();
+            StartCoroutine(cameraShake.Shake(.15f, .4f));
+        }
+    }
+}
